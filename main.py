c7f4e76fcbf23788515eb2af732da76acdaaae3a
aux-dev <sweetpicklz@gmail.com>
2021-06-24 15:24:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import discord
 from dotenv import load_dotenv
 
+prefix = "!"
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -12,4 +14,13 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+if message.author == client.user:
+    return
+
+# BOT COMMANDS
+# !test
+@client.event
+async def on_message(message):
+    if message.content.startsWith(prefix + "test"):
+        await.message.channel.send("test")
 client.run(TOKEN)
