bd24d2ce15e7179913392a25ce086a74596a5d0b
aux-dev <sweetpicklz@gmail.com>
2021-06-24 16:46:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,8 @@ import json
 import discord
 import dotenv
 from dotenv import load_dotenv
-from discord import commands
-from discord import get_prefix
 
-# Prefix
-client = commands.Bot(
-    command_prefix=(get_prefix),
-    )
-@client.event
-async def on_guild_join(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes[str(guild.id)] = '!'
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-@client.event
-async def on_guild_remove(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
+prefix = ['!']
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -41,8 +18,7 @@ async def on_ready():
 # BOT COMMANDS
 # !test
 @client.command
-async def test(message):
-    await message.send("test")
-
+async def test(message, ctx):
+    await ctx.send("test")
 
 client.run(TOKEN)
